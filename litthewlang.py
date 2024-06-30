bf137ffed21d236998d31cd823e874550505a1d2
Matthew Milunic <mmilunicmobile@gmail.com>
2024-06-30 00:10:58 -0400

    sussy imposter typing issue

--- litthewlang.py
+++ litthewlang.py
@@ -6,12 +6,63 @@ from typeguard import typechecked
 
 import sys
 
+import argparse
+
+
+class PrimitiveType(Enum):
+    NUM = 1
+    STR = 2
+    BOOL = 3
+
+class GenderType(Enum):
+    MALE = 1
+    FEMALE = 2
+
+@typechecked 
+class GeneralType():
+    def __init__(self, primitive_type: PrimitiveType, gender_type: GenderType):
+        self.primitive_type = primitive_type
+        self.gender_type = gender_type
+    
+    def get_primitive_type(self) -> PrimitiveType:
+        return self.primitive_type
+    
+    def get_gender_type(self) -> GenderType:
+        return self.gender_type
+    
+    def __repr__(self):
+        return f"{self.primitive_type} {self.gender_type}"
+    
+    def __eq__(self, other):
+        return self.primitive_type == other.primitive_type and self.gender_type == other.gender_type
+
+
+argument_parser = argparse.ArgumentParser(
+                    prog='Litthew Interpreter',
+                    description='This will interpret your Litthewlang programs.',
+                    epilog='This is version v0.2.0')
+
+argument_parser.add_argument('filename')
+argument_parser.add_argument('-t', '--no-type-check',
+                    action='store_true')  # on/off flag
+argument_parser.add_argument('-g', '--no-gender-check',
+                    action='store_true') 
+argument_parser.add_argument('-n', '--no-name-check',
+                    action='store_true')
+
+arguments = argument_parser.parse_args()
+
+DO_SEMANTIC_NAME_CHECK = not arguments.no_gender_check
+DO_TYPE_CHECK = not arguments.no_type_check
+DO_GENDER_CHECK = not arguments.no_gender_check
+
 with open('litthewlang.lark', 'r') as f:
     content = f.read()
 
 parser = lark.Lark(content)
 
-programName = sys.argv[1]
+
+programName = arguments.filename
 
 # basically this contains a dict of all global variable names and their values
 variable_dict_globals = {
@@ -25,15 +76,21 @@ variable_dict_globals = {
 variable_dict_stack = [{}]
 
 # this is a dict of all the global variable types
-variable_dict_globals_types = {}
+variable_dict_globals_types = {
+    "falso": GeneralType(PrimitiveType.BOOL, GenderType.MALE),
+    "truo": GeneralType(PrimitiveType.BOOL, GenderType.MALE),
+    "falsa": GeneralType(PrimitiveType.BOOL, GenderType.FEMALE),
+    "trua": GeneralType(PrimitiveType.BOOL, GenderType.FEMALE)
+}
 
 # these are some lists that include all of certain created types to run type checking and stuff on them in a correct order
-variable_expression_list = []
+typechecks = []
 
 functions_dict_globals = {}
 
 # includes tuples of (name, [param, param, param, return type])
 functions_dict_globals_types = {}
+functions_dict_globals_types_params = {}
 
 string_heap = {}
 num_heap = {}
@@ -87,23 +144,29 @@ class TreeToCode(lark.Transformer):
         }
     
     def function_defo(self, s):
-        return self.function_def(s)
+        return self.function_def(s, GenderType.MALE)
 
-    def function_def(self, s):
-        return FunctionDefenition(s[0]["name"], s[0]["parameters"], ExecutableSequence(s[1:]))
+    def function_def(self, s, gender):
+        return FunctionDefenition(s[0]["name"], s[0]["parameters"], ExecutableSequence(s[1:]), gender)
     
     def assignment(self, s):
-        return ExecutableAssignment(s[0], s[1])
+        val = ExecutableAssignment(s[0], s[1])
+        typechecks.append(val)
+        return val
     
     def creation(self, s):
-        return ExecutableCreation(GeneralType(s[1], s[0]), s[2], s[3])
+        val = ExecutableCreation(GeneralType(s[1], s[0]), s[2], s[3])
+        typechecks.append(val)
+        return val
     
     def function_call(self, s):
         name = s[0]
         args = s[1:]
         if args == [None]:
             args = []
-        return FunctionExpression(s[0], *args)
+        val = FunctionExpression(s[0], *args)
+        typechecks.append(val)
+        return val
     
     def program(self, s):
         return Program(s)
@@ -115,28 +178,6 @@ def to_code(tree):
 # get value (returns base python value)
 # get type (returns the type of the expression) ("num" "str" "bool", "m", "f")
 
-
-class PrimitiveType(Enum):
-    NUM = 1
-    STR = 2
-    BOOL = 3
-
-class GenderType(Enum):
-    MALE = 1
-    FEMALE = 2
-
-@typechecked 
-class GeneralType():
-    def __init__(self, primitive_type: PrimitiveType, gender_type: GenderType):
-        self.primitive_type = primitive_type
-        self.gender_type = gender_type
-    
-    def get_primitive_type(self) -> PrimitiveType:
-        return self.primitive_type
-    
-    def get_gender_type(self) -> GenderType:
-        return self.gender_type
-
 class Expression():
     def get_value(self):
         pass
@@ -144,6 +185,9 @@ class Expression():
         pass
     def get_gender_type(self) -> GenderType:
         pass
+
+    def get_type(self) -> GeneralType:
+        return GeneralType(self.get_primitive_type(), self.get_gender_type())
 
 class Executable():
     def execute(self):
@@ -167,6 +211,9 @@ class PrimitiveExpression(Expression):
     
     def __repr__(self):
         return f"{repr(self.value)} {self.gender_type} {self.primitive_type}"
+    
+    def setScope(self, scope):
+        pass
 
 @typechecked
 class VariableExpression(Expression):
@@ -181,12 +228,21 @@ class VariableExpression(Expression):
             return variable_dict_globals[self.name]
     
     def get_primitive_type(self) -> PrimitiveType:
-        if self.name in self.parentScopeTypes.keys():
+        if (self.name) in self.parentScopeTypes.keys():
             return self.parentScopeTypes[self.name].get_primitive_type()
+        else:
+            assert (self.name) in variable_dict_globals_types.keys()
+            return variable_dict_globals_types[self.name].get_primitive_type()
         
     def get_gender_type(self) -> GenderType:
-        if self.name in self.parentScopeTypes.keys():
+        if (self.name) in self.parentScopeTypes.keys():
             return self.parentScopeTypes[self.name].get_gender_type()
+        else:
+            assert (self.name) in variable_dict_globals_types.keys()
+            return variable_dict_globals_types[self.name].get_gender_type()
+        
+    def setScope(self, scope):
+        self.parentScopeTypes = scope
 
 @typechecked
 class FunctionExpression(Expression, Executable):
@@ -199,14 +255,22 @@ class FunctionExpression(Expression, Executable):
         executor(*self.args)
     
     def get_primitive_type(self) -> PrimitiveType:
-        return functions_dict_globals_types[self.name][-1].get_primitive_type()
+        return functions_dict_globals_types[self.name].get_primitive_type()
     
     def get_gender_type(self) -> GenderType:
-        return functions_dict_globals_types[self.name][-1].get_gender_type()
+        return functions_dict_globals_types[self.name].get_gender_type()
     
     def get_value(self):
         executor = functions_dict_globals[self.name]
         return executor(*self.args)
+    
+    def setScope(self, scope):
+        for arg in self.args:
+            arg.setScope(scope)
+
+    def typecheck(self):
+        for a, b in zip(self.args, functions_dict_globals_types_params[self.name]):
+            assert a.get_type() == b
     
     def __repr__(self):
         return "<" + repr(self.name) + " " + repr(self.args) + ">"
@@ -237,6 +301,15 @@ class ExecutableAssignment(Executable):
     def __repr__(self):
         return "<ExecutableAssignment: " + repr({ "name": self.name, "expression": self.expression}) + ">"
 
+    def setScope(self, scope):
+        self.expression.setScope(scope)
+        self.scope = scope
+
+    def typecheck(self):
+        my_type = self.expression.get_type()
+        other_my_tpye = self.scope[self.name]
+        assert my_type == other_my_tpye
+
 class ExecutableCreation(Executable):
     def __init__(self, general_type, name, expression):
         self.general_type = general_type
@@ -253,13 +326,22 @@ class ExecutableCreation(Executable):
     def __repr__(self):
         return "<ExecutableCreation: " + repr({"general_type": self.general_type, "name": self.name, "expression": self.expression}) + ">"
 
+    def setScope(self, scope):
+        self.expression.setScope(scope)
+
+    def typecheck(self):
+        my_type = self.expression.get_type()
+        other_my_tpye = self.general_type
+        assert my_type == other_my_tpye
+
 class FunctionDefenition():
-    def __init__(self, name, params, executable_sequence):
+    def __init__(self, name, params, executable_sequence, gender: GenderType):
         self.name = name
         self.executable_sequence = executable_sequence
         self.params = params
         self.scope = {}
         self.type_scope = {}
+        self.gender = gender
 
     def executeFunction(self, *args):
         #print(f"Executing function {self.name} {args}")
@@ -279,6 +361,7 @@ class FunctionDefenition():
     
     def fillVariablesScopedExecutionDefinitions(self):
         for param, type_of_param in self.params.items():
+            assert(param not in self.scope.keys())
             creation_type = type_of_param.get_primitive_type()
             if creation_type == PrimitiveType.NUM:
                 self.scope[param] = 0.0
@@ -286,8 +369,11 @@ class FunctionDefenition():
                 self.scope[param] = ""
             elif creation_type == PrimitiveType.BOOL:
                 self.scope[param] = False
+            self.type_scope[param] = type_of_param
+
         for statement in self.executable_sequence.expressions:
             if isinstance(statement, ExecutableCreation):
+                assert(statement.name not in self.scope.keys())
                 creation_type = statement.general_type.get_primitive_type()
                 if creation_type == PrimitiveType.NUM:
                     self.scope[statement.name] = 0.0
@@ -295,6 +381,11 @@ class FunctionDefenition():
                     self.scope[statement.name] = ""
                 elif creation_type == PrimitiveType.BOOL:
                     self.scope[statement.name] = False
+                self.type_scope[statement.name] = statement.general_type
+    
+    def fillOutScope(self):
+        for statement in self.executable_sequence.expressions:
+            statement.setScope(self.type_scope)
 
 
     def __repr__(self):
@@ -309,10 +400,21 @@ class Program():
             if isinstance(statement, FunctionDefenition):
                 assert statement.name not in functions_dict_globals.keys()
                 functions_dict_globals[statement.name] = statement.executeFunction
+                functions_dict_globals_types_params[statement.name] = statement.params
+                output = GeneralType(PrimitiveType.NUM, statement.gender)
+                if ("returno" in statement.type_scope):
+                    output = statement.type_scope["returno"]
+                    #assert(output.get_gender_type() == statement.gender)
+                elif ("returna" in statement.type_scope):
+                    output = statement.type_scope["returna"]
+                    #assert(output.get_gender_type() == statement.gender)
+                functions_dict_globals_types[statement.name] = output
+                
     
     def fillVariableExecutionDefinitions(self):
         for statement in self.statements:
             if isinstance(statement, ExecutableCreation):
+                assert statement.name not in variable_dict_globals.keys()
                 creation_type = statement.general_type.get_primitive_type()
                 if creation_type == PrimitiveType.NUM:
                     variable_dict_globals[statement.name] = 0.0
@@ -320,16 +422,28 @@ class Program():
                     variable_dict_globals[statement.name] = ""
                 elif creation_type == PrimitiveType.BOOL:
                     variable_dict_globals[statement.name] = False
+                variable_dict_globals_types[statement.name] = statement.general_type
 
     def fillVariableScopedExecutionDefinitions(self):
         for statement in self.statements:
             if isinstance(statement, FunctionDefenition):
                 statement.fillVariablesScopedExecutionDefinitions()
     
+    def fillVariableExpressionTypes(self):
+        for statement in self.statements:
+            if isinstance(statement, FunctionDefenition):
+                statement.fillOutScope()
+            else:
+                statement.setScope(variable_dict_globals_types)
+    
     def executeProgram(self):
         for statement in self.statements:
             if isinstance(statement, Executable):
                 statement.execute()
+    
+    def typecheck(self):
+        for statement in typechecks:
+            statement.typecheck()
 
     def __repr__(self):
         return "<Program: " + repr(self.statements) + ">"
@@ -337,7 +451,8 @@ class Program():
 def addSimpleExecutableFunctionDefinition(name: str, evaluer: Any, params: dict):
     assert name not in functions_dict_globals.keys()
     functions_dict_globals[name] = evaluer
-    functions_dict_globals_types[name] = params
+    functions_dict_globals_types[name] = params[-1]
+    functions_dict_globals_types_params[name] = params[:-1]
 
 with open(programName, 'r') as f:
     content = f.read()
@@ -389,31 +504,51 @@ addSimpleExecutableFunctionDefinition("c", lambda x: 0.0, [GeneralType(Primitive
 def heapSub(heap, x, y):
     heap[x] = y
     return 0
+
+def heapGet(heap, x):
+    try:
+        return heap[x]
+    except (KeyError):
+        print("Heap Access Error: You used a variable on the heap that doesn't exist. Womp to the womp.")
 addSimpleExecutableFunctionDefinition("addnum",  lambda x, y: heapSub(num_heap, x.get_value(), y.get_value()), [GeneralType(PrimitiveType.NUM, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
-addSimpleExecutableFunctionDefinition("getnum",  lambda x: num_heap[x.get_value()], [GeneralType(PrimitiveType.NUM, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
+addSimpleExecutableFunctionDefinition("getnum",  lambda x: heapGet(num_heap,x.get_value()), [GeneralType(PrimitiveType.NUM, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
 
 addSimpleExecutableFunctionDefinition("addstr",  lambda x, y: heapSub(string_heap, x.get_value(), y.get_value()), [GeneralType(PrimitiveType.STR, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
-addSimpleExecutableFunctionDefinition("getstr",  lambda x: string_heap[x.get_value()], [GeneralType(PrimitiveType.STR, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
+addSimpleExecutableFunctionDefinition("getstr",  lambda x: heapGet(string_heap, x.get_value()), [GeneralType(PrimitiveType.STR, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
 
 addSimpleExecutableFunctionDefinition("addbool",  lambda x, y: heapSub(bool_heap, x.get_value(), y.get_value()), [GeneralType(PrimitiveType.BOOL, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
-addSimpleExecutableFunctionDefinition("getbool",  lambda x: bool_heap[x.get_value()], [GeneralType(PrimitiveType.BOOL, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
+addSimpleExecutableFunctionDefinition("getbool",  lambda x: heapGet(string_heap, x.get_value()), [GeneralType(PrimitiveType.BOOL, GenderType.MALE), GeneralType(PrimitiveType.NUM, GenderType.MALE)])
 
+#typing
+addSimpleExecutableFunctionDefinition("type", lambda x: str(x.get_type()), [GeneralType(PrimitiveType.NUM, GenderType.MALE), GeneralType(PrimitiveType.STR, GenderType.MALE), GeneralType(PrimitiveType.BOOL, GenderType.MALE)])
 
 # Post-Processsing Steps:
-# * Fill in Variable Types for All Global Creations (ensure name uniqueness) ~ TYPE CHECKING
-# * Fill in Variable Types for All Scoped Function Creations and Parameters (ensure name uniqueness) ~ TYPE CHECKING
-# * Fill in Function Types ~ TYPE CHECKING
 # * Run Through Semantic Name Checking (gender) for Creations and Function Definitions ~ TYPE CHECKING
 # * Ensure Gender Equality ~ TYPE CHECKING
+
+
 # * Run Through Name Checking for All Variables (Check that it exists in a scope) ~ SCOPE CHECKING
 # * Run Through Name Checking for All Functions (Check that it exists) ~ SCOPE CHECKING
-# * Run Type Checking on Functions, Creations, and Assignments (ensure everything is taking the right types) ~ TYPE CHECKING
-# * Fill in Function Execution Definitions ~ CODE EXECUTION
-output_program.fillFunctionExecutionDefinitions()
+
+
 # * Fill in Global Variable Definitions ~ CODE EXECUTION
+# * Fill in Variable Types for All Global Creations (ensure name uniqueness) ~ TYPE CHECKING
 output_program.fillVariableExecutionDefinitions()
+
 # * Fill in Scoped Variable Definitions ~ CODE EXECUTION
+# * Fill in Variable Types for All Scoped Function Creations and Parameters (ensure name uniqueness) ~ TYPE CHECKING
 output_program.fillVariableScopedExecutionDefinitions()
+
+# * Fill in Function Execution Definitions ~ CODE EXECUTION
+# * Fill in Function Types ~ TYPE CHECKING
+output_program.fillFunctionExecutionDefinitions()
+
+# backtrack and let all variable expressions know their type
+output_program.fillVariableExpressionTypes()
+
+# * Run Type Checking on Functions, Creations, and Assignments (ensure everything is taking the right types) ~ TYPE CHECKING
+if (DO_TYPE_CHECK):
+    output_program.typecheck()
 
 #print(output_program)
 
@@ -423,8 +558,7 @@ except (RecursionError):
     print("Recursion Error: You recursed too hard. Go to jail.")
 except (ZeroDivisionError):
     print("Zero Division Error: You divided by zero. Your program was sucked into a black hole.")
-except (KeyError):
-    print("Key Error: You used a variable that doesn't exist. Womp to the womp.")
 
-#print(variable_dict_globals)
-#print(num_heap)
+print("Program Complete")
+# print(variable_dict_globals)
+# print(num_heap)
